4f739345cbb6083df99e16fc8b6052b871e718c9
Sumeet Gajbhar <sumeet.gajbhar@pubmatic.com>
2022-05-10 22:06:13 +0530

    fixed db connection issue

--- config.php
+++ config.php
@@ -1,7 +1,10 @@
 <?php
-   define('DB_SERVER', 'localhost:3036');
-   define('DB_USERNAME', 'u201566448_chemscript_adm');
-   define('DB_PASSWORD', 'Chem@2212');
-   define('DB_DATABASE', 'u201566448_chemscript');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+   $servername = "localhost";
+   $username = "u201566448_chemscript_adm";
+   $password = "Chem@2212";
+   $database = "u201566448_chemscript";
+   
+   $db = mysqli_connect($servername, $username, $password, $database);
+
 ?>
