c2aba88b226609806f04ce3f5c75fb22f397aa2f
Sumeet Gajbhar <sumeet.gajbhar@pubmatic.com>
2022-05-10 21:36:39 +0530

    added login php

---
+++ Config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost:3036');
+   define('DB_USERNAME', 'u201566448_chemscript_adm');
+   define('DB_PASSWORD', 'Chem@2212');
+   define('DB_DATABASE', 'u201566448_chemscript');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
