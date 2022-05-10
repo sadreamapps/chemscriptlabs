8b70fa1f70030dab2b721417391ae480caefb519
Sumeet Gajbhar <sumeet.gajbhar@pubmatic.com>
2022-05-10 21:45:53 +0530

    fixed file names

--- session.php
+++ session.php
@@ -3,12 +3,12 @@
    session_start();
    
    $user_check = $_SESSION['login_user'];
-   
-   $ses_sql = mysqli_query($db,"select username from admin where username = '$user_check' ");
+ 
+   $ses_sql = mysqli_query($db,"select user_name from user_login_details where user_name = '$user_check' ");
    
    $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
    
-   $login_session = $row['username'];
+   $login_session = $row['user_name'];
    
    if(!isset($_SESSION['login_user'])){
       header("location:login.php");
